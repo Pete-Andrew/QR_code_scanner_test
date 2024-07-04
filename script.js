137958d9faaf1befd09055b2ce8d258e5d0c08ea
Peter Andrew <peter.a.andrew@gmail.com>
2024-07-04 20:57:42 +0100

    test - added an element to open the QR code

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-// new code block added below >>>>>>>>>>>>>>>>>>>
-
 function onScanSuccess(decodedText, decodedResult) {
   // Handle on success condition with the decoded text or result.
   console.log(`Scan result: ${decodedText}`, decodedResult);
+  
+  // Redirect to the scanned URL
+  window.location.href = decodedText;
 }
 
 function onScanError(errorMessage) {
-  // handle on error condition, with error message
+  // Handle on error condition, with error message
+  console.error(`Scan error: ${errorMessage}`);
 }
 
 var html5QrcodeScanner = new Html5QrcodeScanner(
-  "reader", { fps: 10, qrbox: 250 });
+  "reader", { fps: 10, qrbox: 250 }
+);
 html5QrcodeScanner.render(onScanSuccess, onScanError);
+
 
 // https://blog.minhazav.dev/research/html5-qrcode.html
 // https://scanapp.org/html5-qrcode-docs/docs/intro
