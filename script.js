63d04bfebbc774e9cbfb2dde689bb8651fbfeb36
Peter Andrew <peter.a.andrew@gmail.com>
2024-07-05 17:14:20 +0100

    complete re-hash of the code following a tutorial

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
-function onScanSuccess(decodedText, decodedResult) {
-  // Handle on success condition with the decoded text or result.
-  console.log(`Scan result: ${decodedText}`, decodedResult);
-  
-  // Redirect to the scanned URL
-  window.location.href = decodedText;
+
+const scanner = new Html5QrcodeScanner('reader', {
+    qrbox: {
+        width: 250,
+        height: 250,
+    },
+    fps: 20,
+});
+
+scanner.render(success, error);
+
+function success(result) {
+    document.getElementById('result').innerHTML = `
+    <h2>Success!</h2>
+    <p><a href="${result}">${result}</a></p>
+    `;
+
+    scanner.clear();
+    document.getElementById('reader').remove();
 }
 
-function onScanError(errorMessage) {
-  // Handle on error condition, with error message
-  console.error(`Scan error: ${errorMessage}`);
-}
+function error(err) {
+    console.error(err);
+};
 
-var html5QrcodeScanner = new Html5QrcodeScanner(
-  "reader", { fps: 10, qrbox: 250 }
-);
-html5QrcodeScanner.render(onScanSuccess, onScanError);
-
-
+// https://www.youtube.com/watch?v=19eo39lqpLM&ab_channel=OpenJavaScript     <<<<<< this link is a tutorial to set this up
 // https://blog.minhazav.dev/research/html5-qrcode.html
 // https://scanapp.org/html5-qrcode-docs/docs/intro
